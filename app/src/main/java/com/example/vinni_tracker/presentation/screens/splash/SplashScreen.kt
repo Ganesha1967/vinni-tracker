b82d7f3703f469b98598ca1de74a7afbb7b634a1
Ganesha <tatiana.gromova2022@yandex.com>
2025-07-02 14:05:48 +0300

    feat(screens): add base UI templates

---
+++ app/src/main/java/com/example/vinni_tracker/presentation/screens/splash/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.vinni_tracker.presentation.screens.splash
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+  Surface(
+    modifier = modifier.fillMaxSize(),
+    color = MaterialTheme.colorScheme.background,
+  ) {
+    Column {
+      Text("This is HomeScreen")
+    }
+  }
+}
